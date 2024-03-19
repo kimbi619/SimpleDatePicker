ddc1fd1322777428e6df873689f34e34703be657
kimbi619 <kimbidarl@gmail.com>
2024-03-19 01:36:39 -0400

    a

--- CustomDatePicker.js
+++ CustomDatePicker.js
@@ -1,54 +1,33 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
-import DateTimePicker from '@react-native-community/datetimepicker'
+import DatetTimePicker from '@react-native-community/datetimepicker'
 
 export default function CustomDatePicker() {
   const [date, setDate] = useState(null)
-  const [showModal, setshowModal] = useState(false)
+  const [showPicker, setShowPicker] = useState(false)
 
   const handleDateChange = (event, selectedDate) => {
-    console.log("---------", event);
-    if (event.type === 'set') {
-      const currentDate = selectedDate || date;
-      setDate(currentDate);
+    if(event.type === 'set'){
+      const currentDate = selectedDate || date
+      console.log(currentDate);
+      setDate(currentDate)
     }
-    if (event.type === 'dismissed') {
-    }
-
-    setshowModal(false);
-  };
-
-  const handleCancel = () => {
-    console.log('cancelling modal');
-    setShowModal(false);
-  };
+    setShowPicker(false)
+  }
   return (
     <View>
-      <TouchableOpacity onPress={() => setshowModal(true)} style={styles.inputContainerStyle}>
+      <TouchableOpacity onPress={() => setShowPicker(true)} style={styles.inputContainerStyle}>
         <Text style={styles.textStyle}>Open Date Picker</Text>
       </TouchableOpacity>
       {
-        showModal && (
-          <DateTimePicker
-            value={date || new Date()}
-            mode={'date'}
+        showPicker && (
+          <DatetTimePicker 
+            mode={'time'}
             is24Hour={true}
-            display="default"
-            confirmBtnText = "Confirm"
-            cancelBtnText="Close"
-            onTouchCancel={handleCancel}
+            value={ date || new Date() }
             onChange={handleDateChange}
-            customStyles={{
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0
-              },
-              dateInput: {
-                marginLeft: 36
-              }
-            }}
+            confirmBtnText="Confirm"
+            cancelBtnText="Dismiss"
           />
         )
       }
